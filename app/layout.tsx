fbbd8c0bce564fcdd9473ffcfc3068f75f91cb78
maebahesioru <pikomaeba@gmail.com>
2026-04-25 21:00:16 +0900

    chore: HKMコイン関連コードを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,7 +77,6 @@ export default function RootLayout({
       </head>
       <body className="antialiased">
         {children}
-              <script src="https://hikakinmaniacoin.hikamer.f5.si/ad.js" data-user-id="cmo8lk1kj0000aggyuhzgv5vk" async></script>
       </body>
     </html>
   );
